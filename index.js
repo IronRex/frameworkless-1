ab6c2922a09d54f7c33dbe5db042287d7bb193a3
Alexander Shnayderman <ashnayderman@forbes.com>
2019-06-03 16:54:50 -0400

    Reading quotes from a file

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 
 const express = require('express');
 
-const script = fs.readFileSync('./scripts/index.js').toString('utf8');
-const style = fs.readFileSync('./styles/index.css').toString('utf8');
+async function main() {
+	const script = fs.readFileSync('./scripts/index.js').toString('utf8');
+	const style = fs.readFileSync('./styles/index.css').toString('utf8');
 
-console.log('script', script);
-console.log('style', style);
+	const quotes = JSON.parse(fs.readFileSync('./data/quotes.json'));
 
-const app = express();
+	const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'pug');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-	res.render('index', {
-		script,
-		style
+	app.get('/', (req, res) => {
+		res.render('index', {
+			quotes,
+			script,
+			style
+		});
 	});
-});
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-});
+	const port = process.env.PORT || 8080;
+	app.listen(port, () => {
+		console.log(`Listening on port ${port}`);
+	});
+}
+
+main();
