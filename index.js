d4e1d10fdc6b7903a0263b5b3d1bcf6dd4e544f3
Alexander Shnayderman <ashnayderman@forbes.com>
2019-06-03 17:50:30 -0400

    Dynamically adding quotes

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 const util = require('util');
 
 const express = require('express');
+const pug = require('pug');
 
 async function main() {
 	const script = fs.readFileSync('./scripts/index.js').toString('utf8');
 	const style = fs.readFileSync('./styles/index.css').toString('utf8');
 
-	const quotes = JSON.parse(fs.readFileSync('./data/quotes.json'));
+	const listItemView = pug.compileFile('./views/item.pug');
+
+	const dataFileName = './data/quotes.json';
+
+	const quotes = JSON.parse(fs.readFileSync(dataFileName));
 
 	const app = express();
 
@@ -24,6 +29,12 @@ async function main() {
 			script,
 			style
 		});
+	});
+
+	app.get('/add', (req, res) => {
+		quotes.push(req.query.quote);
+		res.send(JSON.stringify(listItemView({ quote: req.query.quote })));
+		fs.writeFileSync(dataFileName, JSON.stringify(quotes));
 	});
 
 	const port = process.env.PORT || 8080;
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
-console.log('Hello Console!');
+(function() {
+	const listOfQuotes = document.querySelector('.list-of-quotes');
+	const addButton = document.querySelector('.add-quote-button');
+	const addTextField = document.querySelector('.add-quote-text');
+
+	addButton.addEventListener('click', () => {
+		if(addTextField.value) {
+			fetch(`/add?quote=${addTextField.value}`)
+				.then(response => response.json())
+				.then((html => {
+					listOfQuotes.innerHTML = listOfQuotes.innerHTML += html;
+					addTextField.value = '';
+				}));
+		}
+	});
+})();
