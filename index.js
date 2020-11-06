609ad8909f180dfe8e8a0d835a01f3b63071446c
Alexander Shnayderman <IronRex@gmail.com>
2020-11-06 14:47:08 -0500

    Smaller main function

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const path = require('path');
 const fs = require('fs');
 const fetch = require('node-fetch');
 
+const script = fs.readFileSync('./scripts/index.js').toString('utf8');
+const style = fs.readFileSync('./styles/index.css').toString('utf8');
+
 const express = require('express');
 
+const dataFileName = './data/quotes.json';
+let quotes = JSON.parse(fs.readFileSync(dataFileName));
+
 async function main() {
-	const script = fs.readFileSync('./scripts/index.js').toString('utf8');
-	const style = fs.readFileSync('./styles/index.css').toString('utf8');
-
-	const dataFileName = './data/quotes.json';
-
-	let quotes = JSON.parse(fs.readFileSync(dataFileName));
-
 	const app = express();
 
 	app.set('views', path.join(__dirname, 'views'));
@@ -20,38 +19,43 @@ async function main() {
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 
-	app.get('/', (req, res) => {
-		res.render('index', {
-			quotes,
-			script,
-			style
-		});
-	});
-
-	app.get('/add', (req, res) => {
-		const quote = req.query.quote;
-		res.render('item', { quote: quote });
-		quotes.push(quote);
-		fs.writeFileSync(dataFileName, JSON.stringify(quotes));
-	});
-
-	app.get('/remove', (req, res) => {
-		const quote = req.query.quote;
-		quotes = quotes.filter(it => it !== quote);
-		fs.writeFileSync(dataFileName, JSON.stringify(quotes));
-		res.send('OK');
-	});
-
-	app.get('/fetch', (req, res) => {
-		fetch('https://favqs.com/api/qotd')
-			.then(res => res.json())
-			.then(json => res.send(json.quote.body));
-	});
+	app.get('/', defaultHandler);
+	app.get('/add', addHandler);
+	app.get('/remove', removeHandler);
+	app.get('/fetch', fetchHandler);
 
 	const port = process.env.PORT || 8080;
 	app.listen(port, () => {
 		console.log(`Listening on port ${port}`);
 	});
+}
+
+function defaultHandler(req, res) {
+	res.render('index', {
+		quotes,
+		script,
+		style
+	});
+}
+
+function addHandler(req, res) {
+	const quote = req.query.quote;
+	res.render('item', { quote: quote });
+	quotes.push(quote);
+	fs.writeFileSync(dataFileName, JSON.stringify(quotes));
+}
+
+function removeHandler(req, res) {
+	const quote = req.query.quote;
+	quotes = quotes.filter(it => it !== quote);
+	fs.writeFileSync(dataFileName, JSON.stringify(quotes));
+	res.send('OK');
+}
+
+function fetchHandler(req, res) {
+	fetch('https://favqs.com/api/qotd')
+		.then(res => res.json())
+		.then(json => res.send(json.quote.body));
 }
 
 main();
